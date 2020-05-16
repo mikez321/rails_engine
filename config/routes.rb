d0fddf4024ba6095f124cc77e417c5e9aed6cc39
Mike Hernandez <hernandezmichael321@gmail.com>
2020-05-16 14:58:20 -0600

    Add create to routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :items, only: [:index]
 
-      resources :merchants, only: [:index, :show]
+      resources :merchants, only: [:index, :show, :create]
     end
   end
 end
