71991ea7dd6bbb3b61cf14b3345436ede820c1f5
Mike Hernandez <hernandezmichael321@gmail.com>
2020-05-15 23:38:10 -0600

    Add fakers

--- spec/factories/transactions.rb
+++ spec/factories/transactions.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :transaction do
     invoice_id { "" }
-    credit_card_number { "" }
-    credit_card_expiration_date { "" }
+    credit_card_number { Faker::Number.number(digits: 16) }
+    credit_card_expiration_date { Faker::Number.number(digits: 4) }
     result { "MyString" }
   end
 end
