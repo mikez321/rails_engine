5d4fdff0ecf28352d83929fa4ec6aab6e1be7d3d
Mike Hernandez <hernandezmichael321@gmail.com>
2020-05-16 08:28:21 -0600

    Add dependent options to models

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,7 +1,7 @@
 class Invoice < ApplicationRecord
   belongs_to :merchant
   belongs_to :customer
-  has_many :invoice_items
+  has_many :invoice_items, dependent: :nullify
   has_many :items, through: :invoice_items
-  has_many :transactions
+  has_many :transactions, dependent: :nullify
 end
