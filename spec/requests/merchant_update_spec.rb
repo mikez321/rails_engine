7912aa561ed1c391f0ee000d816e7f832c346819
Mike Hernandez <hernandezmichael321@gmail.com>
2020-05-16 15:17:43 -0600

    Create test for update functionality

---
+++ spec/requests/merchant_update_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+describe 'merchant_api' do
+  it 'can update a merchant' do
+    merchant = create(:merchant)
+    old_name = merchant.name
+
+    body = { name: "Jim's Bikes and Leather" }
+
+    patch "/api/v1/merchants/#{merchant.id}", params: { body: body }
+
+    expect(response).to be_successful
+
+    json = JSON.parse(response.body, symbolize_names: true)
+
+    expect(json[:data][:attributes][:name]).to_not eq(old_name)
+    expect(json[:data][:attributes][:name]).to eq(body[:name])
+  end
+end
