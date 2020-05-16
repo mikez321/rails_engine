3205225e9c99865c67bf6de09c518cea3cf376f7
Mike Hernandez <hernandezmichael321@gmail.com>
2020-05-16 13:33:03 -0600

    Add api format and data tests to spec

--- spec/requests/merchant_spec.rb
+++ spec/requests/merchant_spec.rb
@@ -10,7 +10,24 @@ describe 'merchant API' do
 
     merchants = JSON.parse(response.body)
 
-    expect(merchants.count).to eq(4)
-    expect(merchants.first['name']).to eq(merchant_list.first['name'])
+    expect(merchants['data'].count).to eq(4)
+    expect(merchants['data'].first['attributes']['name']).to eq(merchant_list.first.name)
   end
+
+  it 'is formatted in json:api spec' do
+    merchant_list = create_list(:merchant, 4)
+
+    get '/api/v1/merchants'
+
+    merchants = JSON.parse(response.body)
+
+    expect(merchants).to have_key('data')
+    expect(merchants['data']).to be_an(Array)
+    expect(merchants['data'].length).to eq(4)
+    expect(merchants['data'].first).to have_key('id')
+    expect(merchants['data'].first).to have_key('type')
+    expect(merchants['data'].first).to have_key('attributes')
+    expect(merchants['data'].first['attributes']).to have_key('name')
+  end
+
 end
