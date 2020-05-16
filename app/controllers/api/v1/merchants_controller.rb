f93d7a43f93aa735543a5df17953ad5442187cd7
Mike Hernandez <hernandezmichael321@gmail.com>
2020-05-16 14:58:32 -0600

    Add create method to merchant controller

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -8,4 +8,15 @@ class Api::V1::MerchantsController < ApplicationController
     merchant = Merchant.find(params[:id])
     render json: MerchantSerializer.new(merchant).serializable_hash
   end
+
+  def create
+    merchant = Merchant.create(body_params)
+    render json: MerchantSerializer.new(merchant).serializable_hash
+  end
+
+  private
+
+  def body_params
+    params.require('body').permit(:name)
+  end
 end
