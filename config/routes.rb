82024bccb6c7faf38b628af2323a2f8ba898f8fd
Mike Hernandez <hernandezmichael321@gmail.com>
2020-05-16 14:35:41 -0600

    Update routes to include show pages

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :items, only: [:index]
 
-      resources :merchants, only: [:index]
+      resources :merchants, only: [:index, :show]
     end
   end
 end
