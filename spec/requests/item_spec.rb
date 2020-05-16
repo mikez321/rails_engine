2c4fcb1c0a23208115a244a1dbeef7cff6d6e37b
Mike Hernandez <hernandezmichael321@gmail.com>
2020-05-16 11:39:38 -0600

    Create item api index spec test

--- spec/requests/item_spec.rb
+++ spec/requests/item_spec.rb
@@ -3,10 +3,15 @@ require 'rails_helper'
 describe 'Items API' do
   it 'sends a list of items' do
     merchant = create(:merchant)
-    create_list(:item, 5, merchant_id: merchant.id)
+    item_list = create_list(:item, 5, merchant_id: merchant.id)
 
     get '/api/v1/items'
 
     expect(response).to be_successful
+
+    items = JSON.parse(response.body)
+
+    expect(items.count).to eq(5)
+    expect(items.first['name']).to eq(item_list.first['name'])
   end
 end
