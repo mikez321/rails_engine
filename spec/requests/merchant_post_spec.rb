7e8d660f31bde3b81d39e56cc5027f1a335d1442
Mike Hernandez <hernandezmichael321@gmail.com>
2020-05-16 14:58:12 -0600

    Create test for merchant post

---
+++ spec/requests/merchant_post_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+describe 'merchant_api' do
+  it 'can create a new merchant' do
+    create_list(:merchant, 4)
+    expect(Merchant.count).to eq(4)
+
+    body = { name: "Jim's Bikes and Leather" }
+
+    post '/api/v1/merchants', params: { body: body }
+
+    expect(response).to be_successful
+
+    json = JSON.parse(response.body, symbolize_names: true)
+
+    expect(json[:data][:attributes][:name]).to eq(body[:name])
+
+    expect(Merchant.count).to eq(5)
+  end
+end
