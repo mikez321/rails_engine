6e2c7d228d89247a32a185f6a8708f2ff29578dd
Mike Hernandez <hernandezmichael321@gmail.com>
2020-05-16 20:39:11 -0600

    Break formatting into a new test

--- spec/requests/items/item_api_is_formatted_spec.rb
+++ spec/requests/items/item_api_is_formatted_spec.rb
@@ -1,25 +1,23 @@
-requie 'rails_helper'
+require 'rails_helper'
 
 describe 'item api' do
   it 'is formatted by the json:api standards' do
-    it 'is formatted in json:api spec' do
-      merchant = create(:merchant)
-      item_list = create_list(:item, 5, merchant_id: merchant.id)
+    merchant = create(:merchant)
+    item_list = create_list(:item, 5, merchant_id: merchant.id)
 
-      get '/api/v1/items'
+    get '/api/v1/items'
 
-      merchants = JSON.parse(response.body)
+    merchants = JSON.parse(response.body)
 
-      expect(merchants).to have_key('data')
-      expect(merchants['data']).to be_an(Array)
-      expect(merchants['data'].length).to eq(5)
-      expect(merchants['data'].first).to have_key('id')
-      expect(merchants['data'].first).to have_key('type')
-      expect(merchants['data'].first).to have_key('attributes')
-      expect(merchants['data'].first['attributes']).to have_key('name')
-      expect(merchants['data'].first['attributes']).to have_key('description')
-      expect(merchants['data'].first['attributes']).to have_key('unit_price')
-      expect(merchants['data'].first['attributes']).to have_key('merchant_id')
-    end
+    expect(merchants).to have_key('data')
+    expect(merchants['data']).to be_an(Array)
+    expect(merchants['data'].length).to eq(5)
+    expect(merchants['data'].first).to have_key('id')
+    expect(merchants['data'].first).to have_key('type')
+    expect(merchants['data'].first).to have_key('attributes')
+    expect(merchants['data'].first['attributes']).to have_key('name')
+    expect(merchants['data'].first['attributes']).to have_key('description')
+    expect(merchants['data'].first['attributes']).to have_key('unit_price')
+    expect(merchants['data'].first['attributes']).to have_key('merchant_id')
   end
 end
