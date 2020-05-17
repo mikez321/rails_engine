10f61f6344a0440860abe26156cfa6760c43aa29
Mike Hernandez <hernandezmichael321@gmail.com>
2020-05-17 10:16:54 -0600

    Add nested routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,14 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
-      resources :items, only: [:index, :show, :create, :update, :destroy]
+      resources :items, only: [:index, :show, :create, :update, :destroy] do
+        resources :merchants, only: [:index]
+      end
 
-      resources :merchants, only: [:index, :show, :create, :update, :destroy]
+      resources :merchants, only: [:index, :show, :create, :update, :destroy] do
+        resources :items, only: [:index]
+      end
+
     end
   end
 end
