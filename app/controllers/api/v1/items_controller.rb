f5d168dd4a216a14d6226a4311baabb451706fa4
Mike Hernandez <hernandezmichael321@gmail.com>
2020-05-16 19:29:44 -0600

    Add show method to items controller

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,6 +1,17 @@
 class Api::V1::ItemsController < ApplicationController
   def index
     items = Item.all
-    render json: ItemSerializer.new(items).serializable_hash
+    render_json(items)
+  end
+
+  def show
+    item = Item.find(params[:id])
+    render_json(item)
+  end
+
+  private
+
+  def render_json(item)
+    render json: ItemSerializer.new(item).serializable_hash
   end
 end
