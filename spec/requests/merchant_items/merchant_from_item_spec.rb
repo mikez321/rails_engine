57ed40205eb49899df8e819f63046368256ac7e7
Mike Hernandez <hernandezmichael321@gmail.com>
2020-05-17 11:39:32 -0600

    Update test

--- spec/requests/merchant_items/merchant_from_item_spec.rb
+++ spec/requests/merchant_items/merchant_from_item_spec.rb
@@ -4,8 +4,8 @@ describe 'searching through relationships' do
   it 'can return all items associated with a merchant' do
     merchant1 = create(:merchant)
       item1 = create(:item, merchant_id: merchant1.id)
+      item2 = create(:item, merchant_id: merchant1.id)
     merchant2 = create(:merchant)
-      item2 = create(:item, merchant_id: merchant2.id)
       item3 = create(:item, merchant_id: merchant2.id)
       item4 = create(:item, merchant_id: merchant2.id)
       item5 = create(:item, merchant_id: merchant2.id)
@@ -18,8 +18,10 @@ describe 'searching through relationships' do
     json = JSON.parse(response.body, symbolize_names: true)
 
     expect(Item.count).to eq(5)
-    expect(json[:data].first[:type]).to eq('merchant')
-    expect(json[:data].first[:id]).to eq(merchant1.id.to_s)
-    expect(json[:data].count).to eq(1)
+    expect(json[:data].keys).to include(:id)
+    expect(json[:data].keys).to include(:type)
+    expect(json[:data].keys).to include(:attributes)
+    expect(json[:data][:type]).to eq('merchant')
+    expect(json[:data][:id]).to eq(merchant1.id.to_s)
   end
 end
