adc9e0c534ec16a93100cbce2caeb3358a994ffa
Mike Hernandez <hernandezmichael321@gmail.com>
2020-05-17 11:24:20 -0600

    Add filter to item show method

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,6 +1,10 @@
 class Api::V1::ItemsController < ApplicationController
   def index
-    items = Item.all
+    if params[:merchant_id]
+      items = Item.where(merchant_id: params[:merchant_id])
+    else
+      items = Item.all
+    end
     render_json(items)
   end
 
