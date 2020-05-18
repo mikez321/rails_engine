6a48f8833d9d6ee0081c16a2e535dc3f4ceeacc7
Mike Hernandez <hernandezmichael321@gmail.com>
2020-05-18 15:48:43 -0600

    Create test for single finder items

---
+++ spec/requests/finders/find_items_through_params_spec.rb
@@ -0,0 +1,108 @@
+require 'rails_helper'
+
+describe 'finding records through request' do
+  it 'can find an item based on its attributes' do
+    merchant1 = create(:merchant)
+      item1 = create(:item, merchant_id: merchant1.id)
+      item2 = create(:item, merchant_id: merchant1.id)
+    merchant2 = create(:merchant)
+      item3 = create(:item, merchant_id: merchant2.id)
+      item4 = create(:item, merchant_id: merchant2.id)
+
+      item5 = Item.create(name: 'Oakley Sutro Sunglasses',
+                          description: 'They are big and badass',
+                          unit_price: 139.99,
+                          merchant_id: merchant2.id)
+
+
+    get "/api/v1/items/find?name=Oakley"
+
+    expect(response).to be_successful
+
+    json = JSON.parse(response.body, symbolize_names: true)
+
+    expect(json[:data].length).to eq(1)
+    expect(json[:data].first[:id]).to eq(item5.id.to_s)
+  end
+
+  it 'can find an item by part of its attributes' do
+    merchant2 = create(:merchant)
+      item3 = create(:item, merchant_id: merchant2.id)
+      item4 = create(:item, merchant_id: merchant2.id)
+
+      item5 = Item.create(name: 'Oakley Sutro Sunglasses',
+                          description: 'They are big and badass',
+                          unit_price: 139.99,
+                          merchant_id: merchant2.id)
+
+
+    get "/api/v1/items/find?name=glasses"
+
+    expect(response).to be_successful
+
+    json = JSON.parse(response.body, symbolize_names: true)
+
+    expect(json[:data].first[:id]).to eq(item5.id.to_s)
+  end
+
+  it 'can find an item and is case insensitive' do
+    merchant2 = create(:merchant)
+      item3 = create(:item, merchant_id: merchant2.id)
+      item4 = create(:item, merchant_id: merchant2.id)
+
+      item5 = Item.create(name: 'Oakley Sutro Sunglasses',
+                          description: 'They are big and badass',
+                          unit_price: 139.99,
+                          merchant_id: merchant2.id)
+
+
+    get "/api/v1/items/find?name=sutr"
+
+    expect(response).to be_successful
+
+    json = JSON.parse(response.body, symbolize_names: true)
+
+    expect(json[:data].length).to eq(1)
+    expect(json[:data].first[:id]).to eq(item5.id.to_s)
+  end
+
+  it 'can find an item by description' do
+    merchant2 = create(:merchant)
+      item3 = create(:item, merchant_id: merchant2.id)
+      item4 = create(:item, merchant_id: merchant2.id)
+
+      item5 = Item.create(name: 'Oakley Sutro Sunglasses',
+                          description: 'They are big and badass',
+                          unit_price: 139.99,
+                          merchant_id: merchant2.id)
+
+
+    get "/api/v1/items/find?description=badass"
+
+    expect(response).to be_successful
+
+    json = JSON.parse(response.body, symbolize_names: true)
+    expect(json[:data].length).to eq(1)
+    expect(json[:data].first[:id]).to eq(item5.id.to_s)
+  end
+
+  # it 'can find an item by price' do
+  #   merchant2 = create(:merchant)
+  #     item3 = create(:item, merchant_id: merchant2.id)
+  #     item4 = create(:item, merchant_id: merchant2.id)
+  #
+  #     item5 = Item.create(name: 'Oakley Sutro Sunglasses',
+  #                         description: 'They are big and badass',
+  #                         unit_price: 139.99,
+  #                         merchant_id: merchant2.id)
+  #
+  #
+  #   get "/api/v1/items/find?unit_price=139"
+  #
+  #   expect(response).to be_successful
+  #
+  #   json = JSON.parse(response.body, symbolize_names: true)
+  #   expect(json[:data].length).to eq(1)
+  #   expect(json[:data].first[:id]).to eq(item5.id.to_s)
+  # end
+end
