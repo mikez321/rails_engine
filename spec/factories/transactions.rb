49c60f416f84146e291076d2bd705b52750ee94c
Mike Hernandez <hernandezmichael321@gmail.com>
2020-05-19 21:03:40 -0600

    Add default data to factories and remove stuff from test

--- spec/factories/transactions.rb
+++ spec/factories/transactions.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :transaction do
-    invoice_id
+    invoice_id {rand(1..10)}
     credit_card_number { Faker::Number.number(digits: 16) }
     credit_card_expiration_date { Faker::Number.number(digits: 4) }
-    result 
+    result
   end
 end
