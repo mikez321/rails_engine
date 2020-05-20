109e143aa0fa4826eef8e360e64cde7feb9e108f
Mike Hernandez <hernandezmichael321@gmail.com>
2020-05-20 15:21:37 -0600

    Update merchant find to include only successful transactions

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,10 +1,13 @@
 class Merchant < ApplicationRecord
   has_many :items, dependent: :destroy
   has_many :invoices, dependent: :destroy
+  has_many :transactions, through: :invoices
   has_many :invoice_items, through: :items
 
   def self.most_items(merchants_requested)
     Merchant.joins(:invoice_items)
+            .joins(:transactions)
+            .where(transactions: { result: 'success'} )
             .group(:id)
             .select('merchants.id, merchants.name, sum(invoice_items.quantity) as total_sold')
             .order('total_sold desc')
