40e82f6da2a68283fe4b607f637aba59fcf02238
Mike Hernandez <hernandezmichael321@gmail.com>
2020-05-20 22:01:44 -0600

    Refactor item search controller and create model methods and tests

--- app/controllers/api/v1/items/item_search_controller.rb
+++ app/controllers/api/v1/items/item_search_controller.rb
@@ -1,15 +1,11 @@
 class Api::V1::Items::ItemSearchController < ApplicationController
   def index
-    item = Item.where("LOWER(#{item_params.keys.first}) LIKE ?", "%#{item_params.values.first.downcase}%")
+    item = Item.all_with_params(item_params)
     item_json(item)
   end
 
   def show
-    if item_params.keys.length > 1
-      item = Item.multi_find(item_params)
-    else
-      item = Item.find_by("LOWER(#{item_params.keys.first}) LIKE ?", "%#{item_params.values.first.downcase}%")
-    end
+    item = Item.find_with_params(item_params)
     item_json(item)
   end
 
--- app/models/item.rb
+++ app/models/item.rb
@@ -3,8 +3,26 @@ class Item < ApplicationRecord
   has_many :invoice_items, dependent: :destroy
   has_many :invoices, through: :invoice_items
 
-  def self.multi_find(item_params)
+  def self.find_with_multi_params(item_params)
     Item.where("#{item_params.keys.first} LIKE ?", "%#{item_params.values.first}%")
         .find_by("#{item_params.keys.last} LIKE ?", "%#{item_params.values.last}%")
+  end
+
+  def self.all_with_params(item_params)
+    Item.where("LOWER(#{item_params.keys.first}) LIKE ?",
+               "%#{item_params.values.first.downcase}%")
+  end
+
+  def self.find_with_single_param(item_params)
+    Item.find_by("LOWER(#{item_params.keys.first}) LIKE ?",
+                 "%#{item_params.values.first.downcase}%")
+  end
+
+  def self.find_with_params(item_params)
+    if item_params.keys.length > 1
+      item = Item.find_with_multi_params(item_params)
+    else
+      item = Item.find_with_single_param(item_params)
+    end
   end
 end
